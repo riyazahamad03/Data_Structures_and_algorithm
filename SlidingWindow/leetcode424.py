537ca8be4173eda3d09df3ab77bb07d0fbd0c26a
riyazahamad03 <riyazahameddo@gmail.com>
2022-12-31 14:13:32 +0530

    added leetcode solution

---
+++ SlidingWindow/leetcode424.py
@@ -0,0 +1,15 @@
+class solution:
+    def charReplacement(self,s:str,k:int):
+        d,l = {},0
+        maxf,res = 0,0
+        for r in range(len(s)):
+            d[s[r]] = 1 + d.get(s[r],0)
+            maxf = max(maxf,d[s[r]])
+
+            while (r-l+1) - maxf > k:
+                d[s[l]] -= 1
+                l+=1
+            res = max(res,r-l+1)
+        return res
+x = solution()
+print(x.charReplacement("ABAB",2))
