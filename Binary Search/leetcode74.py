6319d2e302ea664e82c1c911182b56f3bfd3785f
riyazahamad03 <riyazahameddo@gmail.com>
2023-01-17 14:37:42 +0530

    added solution

---
+++ Binary Search/leetcode74.py
@@ -0,0 +1,30 @@
+class solution:
+    def search2D(self,matrix:list[list[int]],target:int):
+        rows,cols = len(matrix),len(matrix[0])
+        # finding the target row
+        top,bottom = 0,rows-1
+        while top <= bottom:
+            row = (top + bottom)//2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bottom = row - 1
+            else:
+                break
+        if not (top<=bottom):
+            return False
+        
+        # doing binary search in the column
+        l,r = 0,cols-1
+        row = (top + bottom)//2
+        while l<=r:
+            mid = (l + r )//2
+            if target > matrix[row][mid]:
+                l = mid + 1
+            elif target < matrix[row][mid]:
+                r = mid - 1
+            else:
+                return True
+        return True
+x = solution()
+print(x.search2D([[1,3,5,7],[10,11,16,20],[23,30,34,60]],121))
