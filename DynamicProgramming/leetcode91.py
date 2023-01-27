299a6dc9a6b5920987ae8eb550de2f9b4756132f
riyazahamad03 <riyazahameddo@gmail.com>
2023-01-27 21:26:29 +0530

    added leetcode solution

---
+++ DynamicProgramming/leetcode91.py
@@ -0,0 +1,19 @@
+# 91. Decode Ways
+class solution:
+    def decodeWays(self,s:str):
+        dp = {len(s) : 1}
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if s[i] == "0":
+                return 0
+            res = dfs(i  + 1)
+            if (i + 1 < len(s) and (s[i]=="1" or s[i]=="2" and s[i+1] in "0123456")):
+                res += dfs(i + 2)
+            dp[i] = res
+            return res
+        return dfs(0) 
+
+
+x = solution()
+print(x.decodeWays("12"))
