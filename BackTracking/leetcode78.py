a57031316dd6a9ce08dce547f0dcdadc742a3878
riyazahamad03 <riyazahameddo@gmail.com>
2023-02-28 22:38:25 +0530

    added leetcode solution

---
+++ BackTracking/leetcode78.py
@@ -0,0 +1,16 @@
+class solution:
+    def subSet(self,nums:list[int]):
+        res=[]
+        sub = []
+        def dfs(i):
+            if i>=len(nums):
+                res.append(sub.copy())
+                return
+            sub.append(nums[i])
+            dfs(i + 1)
+            sub.pop()
+            dfs(i + 1)
+        dfs(0)
+        return res
+x = solution()
+print(x.subSet([1,2,3]))
