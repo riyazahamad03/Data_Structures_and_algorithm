14e33545fc93eac28e514ee87636da220b0eedad
riyazahamad03 <riyazahameddo@gmail.com>
2023-03-22 09:18:22 +0530

    added dfs solution

--- unionFindAlgorithm/leetcode323.py
+++ unionFindAlgorithm/leetcode323.py
@@ -7,6 +7,27 @@
 
 # Input : n = 5 and edges = [[0, 1], [1, 2], [3, 4]]
 # Output: 2
+
+class solution:
+    def countComponents(self,n:int,edges:list[list[int]]):
+        hashMap = {i : [] for i in range(n)}
+        for i,e in edges:
+            hashMap[i].append(e)
+        visit = set()
+        
+        def dfs(i):
+            if i in visit:
+                return True
+            visit.add(i)
+            for idex in hashMap[i]:
+                dfs(idex)
+            return True
+        res = 0
+        for i in range(n):
+            if i not in visit and dfs(i):
+                res += 1
+        return res
+
 class UnionFind:
     def __init__(self):
         self.hashTable = {}
@@ -14,7 +35,6 @@ class UnionFind:
         y = self.hashTable.get(x,x)
         if x!=y:
             y = self.hashTable[x] = self.parent(y)
-        # print(y)
         return y
     def union(self,x,y):
         self.hashTable[self.parent(x)] = self.parent(y)
@@ -50,7 +70,45 @@ class UnionFindApproach2:
         res = n
         for a,b in edges:
             res -= d.UnionOperation(a,b,lst)
-        return res 
+        return res
+
+class sol3:
+    def countComponents(self,n:int,edges:list[list[int]]):
+        parents = [i for i in range(n)]
+        rank = [[1] for _ in range(n)]
+
+        def findPar(n1):
+            res = n1
+            while res != parents[res]:
+                parents[res] = parents[parents[res]]
+                res = parents[res]
+            return res
+        def union(n1 , n2):
+            p1 , p2 = findPar(n1) , findPar(n2)
+            
+            if p1 == p2:
+                return 0
+            
+            if rank[p2] < rank[p1]:
+                parents[p1] = p2
+                rank[p2] += rank[p1]
+            else:
+                parents[p2] = p1
+                rank[p1] += rank[p2]
+            return 1
+        res = n
+        for n1 , n2 in edges:
+            res -= union(n1 , n2)
+        return res
+            
+
+
 
 Y = UnionFindApproach2()
 print("Approach 2 : ",Y.countComponents(5,[[0, 1], [1, 2], [3, 4]]))
+
+x = solution()
+print(x.countComponents(7,[[0, 1], [1, 2], [3, 4], [5, 6]]))
+
+x = sol3()
+print(x.countComponents(7,[[0, 1], [1, 2], [3, 4], [5, 6]]))
