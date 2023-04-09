69949c1c005435e48fad599f710b04c0041292fb
riyazahamad03 <riyazahameddo@gmail.com>
2023-04-09 19:37:33 +0530

    added leetcode solution

--- Tree/leetcode513.py
+++ Tree/leetcode513.py
@@ -1,16 +1,18 @@
 import collections
+
+
 class TreeNode:
-    def __init__(self,data):
+    def __init__(self, data):
         self.val = data
         self.left = None
         self.right = None
-        
+
 
 class Tree:
     def __init__(self):
         self.root = None
 
-    def insert(self,data):
+    def insert(self, data):
 
         newNode = TreeNode(data)
         if (self.root == None):
@@ -19,20 +21,21 @@ class Tree:
             currNode = self.root
             while True:
                 if (data < currNode.val):
-                    if(currNode.left == None):
+                    if (currNode.left == None):
                         currNode.left = newNode
                         return
                     else:
                         currNode = currNode.left
-                elif(data > currNode.val):
-                    if(currNode.right == None):
+                elif (data > currNode.val):
+                    if (currNode.right == None):
                         currNode.right = newNode
                         return
                     else:
                         currNode = currNode.right
 
+
 class solution:
-    def findBottomLeft(self,root:TreeNode):
+    def findBottomLeft(self, root: TreeNode):
         q = collections.deque()
         q.append(root)
         while q:
@@ -44,6 +47,7 @@ class solution:
                     q.append(node.left)
         return node.val
 
+
 root = Tree()
 root.insert(1)
 root.insert(2)
@@ -52,4 +56,3 @@ root.insert(4)
 
 x = solution()
 print(x.findBottomLeft(root.root))
-
