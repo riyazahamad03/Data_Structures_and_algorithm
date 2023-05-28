584fdda05218eccf631ca0d3db07f38496df510c
riyazahamad03 <riyazahameddo@gmail.com>
2023-05-28 16:03:19 +0530

    added leetcode solution

---
+++ DynamicProgramming/leetcode1547.py
@@ -0,0 +1,17 @@
+class solution:
+    def minCost(self , n : int , cuts : list[int]):
+        dp = {}
+        def dfs(l , r):
+            if r - l == 1:
+                return 0
+            if (l , r) in dp:
+                return dp[(l , r)]
+            res = float("inf")
+            for c in cuts:
+                if l < c < r:
+                    res = min(res , (r - l) + dfs(l , c) + dfs(c , r))
+            dp[(l , r)] = res = 0 if res == float("inf") else res
+            return res
+        return dfs(0 , n)
+x = solution()
+print(x.minCost(7 , [1,3,4,5]))
