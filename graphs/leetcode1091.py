ed26021347583835ca2eb737f338bd9bcbe91d86
riyazahamad03 <riyazahameddo@gmail.com>
2023-06-01 19:56:56 +0530

    added leetcode solution

--- graphs/leetcode1091.py
+++ graphs/leetcode1091.py
@@ -1,22 +1,32 @@
 import collections
+
+
 class solution:
-    def shortestPathBinaryMatrix(self , grid : list[list[int]]):
-        ROWS,COLS = len(grid) , len(grid[0])
-        q = collections.deque([(0 , 0 , 1)]) #[row , col , length]
-        visit = set((0 , 0))
-        directions = [[1 , 0],[0 , 1],[0 , -1],[-1 , 0],
-                      [1 , 1],[1 , -1],[-1, 1],[-1 , -1]]
+    def shortestPathBinaryMatrix(self, grid: list[list[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        q = collections.deque()
+        q.append((0, 0, 1))
+        visit = set((0, 0))
+        directions = [[1, 0], [0, 1], [0, -1], [-1, 0],
+                      [1, 1], [1, -1], [-1, 1], [-1, -1]]
+
+        def isValid(r, c):
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] == 1:
+                return False
+            return True
         while q:
-            row,col,length = q.popleft()
-            if(row < 0 or row >= ROWS or col < 0 or col >=COLS or grid[row][col]==1):
-                continue
-            if row == ROWS-1 and col==COLS-1:
-                return length
-            for dr , dc in directions:
-                r,c = row+dr , col + dc
-                if((r,c) not in visit and r in range(ROWS) and c in range(COLS)): #row and col checking is not necessary because we are ignoring in the above if condition
-                    visit.add((r,c))
-                    q.append((r , c , length + 1))
+            for i in range(len(q)):
+                r, c, length = q.popleft()
+                if not isValid(r, c):
+                    continue
+                if r == ROWS-1 and c == COLS-1:
+                    return length
+                for dr, dc in directions:
+                    if (r + dr, c + dc) not in visit:
+                        q.append((r + dr, c + dc, length + 1))
+                        visit.add((r + dr, c + dc))
         return -1
+
+
 x = solution()
-print(x.shortestPathBinaryMatrix([[0,0,0],[1,1,0],[1,1,0]]))
+print(x.shortestPathBinaryMatrix([[0, 0, 0], [1, 1, 0], [1, 1, 0]]))
