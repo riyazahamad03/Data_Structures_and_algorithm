f3c8d8f793c9e775383d99447a7c00e3a6f2d9f8
riyazahamad03 <riyazahameddo@gmail.com>
2023-07-13 07:02:22 +0530

    added leetcode solution

--- graphs/leetcode207.py
+++ graphs/leetcode207.py
@@ -1,30 +1,33 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
-        preMap = {i : [] for i in range(numCourses)}
-        
-        for idex in prerequisites:
-            preMap[idex[0]].append(idex[1]) 
-        
-        visitSet = set()
-        def dfs(crs):
-            if crs in visitSet:
-                return False
-            if preMap[crs] == []:
-                return True
-            
-            visitSet.add(crs)
+        preMap = {i: [] for i in range(numCourses)}
 
-            for c in preMap[crs]:
-                if not dfs(c):
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visit = set()
+
+        def dfs(crs):
+            if crs in visit:
+                return False
+            if not preMap[crs]:
+                return True
+
+            visit.add(crs)
+
+            for course in preMap[crs]:
+                if not dfs(course):
                     return False
-            
-            visitSet.remove(crs)
+
+            visit.remove(crs)
             preMap[crs] = []
             return True
-            
+
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
         return True
+
+
 x = Solution()
-print(x.canFinish(2,[[1,0],[0,1]]))
+print(x.canFinish(2, [[1, 0], [0, 1]]))
