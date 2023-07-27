33d1caf656cac8f505270d68d272016bcd1091e3
riyazahamad03 <riyazahameddo@gmail.com>
2023-07-27 19:24:23 +0530

    added leetcode solution

---
+++ Binary Search/leetcode2141.py
@@ -0,0 +1,17 @@
+class solution:
+    def maxRunTime(self, n, batteries):
+        l, r = 1, sum(batteries)
+        while l < r:
+            m = r - (r - l)//2
+            extra = 0
+            for bat in batteries:
+                extra += min(bat, m)
+            if extra >= n * m:
+                l = m
+            else:
+                r = m - 1
+        return l
+
+
+x = solution()
+print(x.maxRunTime(n=2, batteries=[3, 3, 3]))
