731ba9c525bdb0c8557473951603722559b68b60
RIYAZ AHAMED <riyazahamad397@gmail.com>
2024-02-25 21:35:56 +0530

    added leetcode solution

---
+++ graphs/leetcode2709.py
@@ -0,0 +1,51 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.size = [1] * n
+        self.count = n
+
+    def find(self, x):
+        if self.par[x] != x:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+
+        if px == py:
+            return
+        if self.size[px] < self.size[py]:
+            self.par[px] = self.par[py]
+            self.size[py] += self.size[px]
+        else:
+            self.par[py] = self.par[px]
+            self.size[px] += self.size[py]
+        self.count -= 1
+
+
+class Solution:
+    def canTraverseAllPairs(self, nums: list[int]) -> bool:
+        uf = UnionFind(len(nums))
+        factor_index = {}
+        for i, n in enumerate(nums):
+            f = 2
+            while f * f <= n:
+                if n % f == 0:
+                    if f in factor_index:
+                        uf.union(i, factor_index[f])
+                    else:
+                        factor_index[f] = i
+
+                    while n % f == 0:
+                        n = n // f
+                f += 1
+            if n > 1:
+                if n in factor_index:
+                    uf.union(i, factor_index[n])
+                else:
+                    factor_index[n] = i
+        return uf.count == 1
+
+
+x = Solution()
+print(x.canTraverseAllPairs([2, 3, 6]))
