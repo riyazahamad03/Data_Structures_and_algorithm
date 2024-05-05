9bcf1d0cad51dc9092ec451b4289a6ff6525ab28
RIYAZ AHAMED <riyazahamad397@gmail.com>
2024-05-05 06:18:05 +0530

    added leetcode solution

---
+++ LinkedList/leetcode237.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        return head
+
+
+x = Solution()
+head = ListNode(4)
+head.next = ListNode(5)
+head.next.next = ListNode(1)
+head.next.next.next = ListNode(9)
+
+
+node = head.next
+print(x.deleteNode(node))
