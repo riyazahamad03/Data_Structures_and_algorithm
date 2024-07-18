f64d7ae5646ecef38ecd7f9b8b678d27f7783bc9
RIYAZ AHAMED <riyazahamad397@gmail.com>
2024-07-18 21:16:48 +0530

    added leetcode solution

---
+++ Tree/leetcode1530.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def countPairs(self, root: TreeNode, distance: int) -> int:
+        self.pairs = 0
+
+        def dfs(node):
+            if not node:
+                return defaultdict(int)
+
+            if not node.left and not node.right:
+                count = defaultdict(int)
+                count[1] = 1  # Distance 1 for leaf node
+                return count
+
+            left_dist = dfs(node.left)
+            right_dist = dfs(node.right)
+
+            for d1 in left_dist:
+                for d2 in right_dist:
+                    if d1 + d2 <= distance:
+                        self.pairs += left_dist[d1] * right_dist[d2]
+
+            all_dist = defaultdict(int)
+
+            for d in left_dist:
+                if d + 1 <= distance:
+                    all_dist[d + 1] = left_dist[d]
+            for d in right_dist:
+                if d + 1 <= distance:
+                    all_dist[d + 1] += right_dist[d]
+
+            return all_dist
+
+        dfs(root)
+        return self.pairs
+
+
+x = Solution()
+treeNode = TreeNode(1)
+treeNode.left = TreeNode(3)
+treeNode.right = TreeNode(5)
+
+print(x.countPairs(treeNode , 5))
