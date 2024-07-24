1635ef562d31c4c2a403600b1dcdde8bd41a00e0
RIYAZ AHAMED <riyazahamad397@gmail.com>
2024-07-24 21:41:18 +0530

    added leetcode solution

---
+++ Arrays/leetcode2191.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortJumbled(self, mapping, nums):
+        store_pairs = []
+        for i in range(len(nums)):
+            number = str(nums[i])
+            formed = ""
+            for j in range(len(number)):
+                formed = formed + str(mapping[int(number[j])])
+            mapped_value = int(formed)
+            store_pairs.append((mapped_value, i))
+
+        store_pairs.sort()
+        answer = []
+        for pair in store_pairs:
+            answer.append(nums[pair[1]])
+        return answer
+
+
+x = Solution()
+print(x.sortJumbled(mapping=[8, 9, 4, 0, 2, 1, 3, 5, 7, 6], nums=[991, 338, 38]))
